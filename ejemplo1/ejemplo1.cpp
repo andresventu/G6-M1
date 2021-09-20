ea5fe243b78041f7fd0dd49ce0f41966cfe94483
andresventu <andresventuravaliente@gmail.com>
2021-09-20 16:29:48 +0200

    Codigo comentado.

--- ejemplo1/ejemplo1.cpp
+++ ejemplo1/ejemplo1.cpp
@@ -12,6 +12,7 @@ ejemplo1::ejemplo1(): Ui_Counter()
     timer.start(num_frecuencia);
 
 }
+//Muestra el contador por pantalla en el lcdNumber y lo aumenta
 void ejemplo1::ActivacionTimer(){
     lcdNumber->display(contador);
     contador++;
@@ -23,9 +24,12 @@ void ejemplo1::doButton()
 
     timer.stop();
 }
+//Al pulsar en el boton start, se actualiza la variable num_frecuencia
+//con el nuevo valor del dial y se inicia de nuevo el contador con la
+//nueva frecuencia o periodo
 void ejemplo1::doButtonStart()
 {
-    dial->sliderPosition();
+
     num_frecuencia=dial->sliderPosition();
     frecuencia->display(num_frecuencia);
 
